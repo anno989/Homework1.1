a7ce4ea28b419116a1c1837595ebe092d00874d2
Aleksandr <a.v.agafonov@mail.ru>
2022-12-16 01:04:45 +0300

    Task1 solve

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine ("Введите первое число ");
+﻿Console.WriteLine ("Введите первое число  ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int b = int.Parse(Console.ReadLine());
