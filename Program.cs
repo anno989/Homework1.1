5498cabade5dc10f02f39e9dbc5c5b38dfd10d45
Aleksandr <a.v.agafonov@mail.ru>
2022-12-15 23:36:40 +0300

    Task solve

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+  Console.WriteLine ("max =" + a);
+   Console.WriteLine ("min =" + b);
+}
+else if (a == b)
+{
+  Console.WriteLine ("max = min = " +a);
+}  
+else
+{
+ Console.WriteLine ("max =" + b);
+   Console.WriteLine ("min =" + a);   
+}
